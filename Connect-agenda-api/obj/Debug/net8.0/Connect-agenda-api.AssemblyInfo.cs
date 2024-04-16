6d47ea61f148651999e3a2882dbbe34c33c932a3
gutsz136 <gjose2980@gmail.com>
2024-04-16 19:31:54 -0300

    Arrumando Login

--- Connect-agenda-api/obj/Debug/net8.0/Connect-agenda-api.AssemblyInfo.cs
+++ Connect-agenda-api/obj/Debug/net8.0/Connect-agenda-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect-agenda-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df26575aaaee81d365cdb42532024e169d0a138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c4759c994cbc4c51b0ef47c3e55b7d1b4d758a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect-agenda-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect-agenda-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
