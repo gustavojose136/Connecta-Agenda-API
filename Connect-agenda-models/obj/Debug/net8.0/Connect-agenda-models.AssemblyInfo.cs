06b316b7b7d26da8a3230bb81da1d4c10f64759a
gutsz136 <gjose2980@gmail.com>
2024-06-24 11:55:29 -0300

    d

--- Connect-agenda-models/obj/Debug/net8.0/Connect-agenda-models.AssemblyInfo.cs
+++ Connect-agenda-models/obj/Debug/net8.0/Connect-agenda-models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect-agenda-models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb023e80603c8d5930c0523b54b07680860f5cff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f998af17020846f099b93da3f2db19fbf7358266")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect-agenda-models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect-agenda-models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
